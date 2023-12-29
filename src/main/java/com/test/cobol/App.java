0dc41d949b3f66b26f2fefc228fba14be3b2fb3a
jaysara <saraiya.jay@gmail.com>
2023-12-28 19:43:11 -0600

    fixing pom.xml for java11

--- src/main/java/com/test/cobol/App.java
+++ src/main/java/com/test/cobol/App.java
@@ -13,6 +13,7 @@ public class App
     public static void main( String[] args )
     {
         App app = new App();
+        System.out.println("Current JVM version - " + System.getProperty("java.version"));
         app.start();
     }
 
